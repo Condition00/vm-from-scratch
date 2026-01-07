5f7420e2169787bd9548c702d619e0a50a4fd28a
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-07 19:23:06 +0530

    value arrays
    loading constansts by using dynamic Array Value

--- chunk.c
+++ chunk.c
@@ -1,16 +1,21 @@
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 #include <stdlib.h>
 
 void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  // When we initialize a new chunk, we initialize its constant list too.
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
   initChunk(chunk);
+  // likewise we free the constants when we free the chunk
+  freeValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk *chunk, uint8_t byte) {
@@ -23,4 +28,11 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
 
   chunk->code[chunk->count] = byte;
   chunk->count++;
+}
+
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
+  // After we add the constant, we return the index where the constant was
+  // appended so that we can locate that same constant later.
 }
--- chunk.h
+++ chunk.h
@@ -5,6 +5,8 @@
 #include "memory.h"
 #include <stdint.h>
 
+#include "value.h"
+
 typedef enum {
   OP_RETURN,
 } Opcode;
@@ -13,9 +15,13 @@ typedef struct {
   int count;
   int capacity;
   uint8_t *code;
+  ValueArray constants;
 } Chunk;
+
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte);
+// convenience method to add a new constant to the chunk.
+int addConstant(Chunk *chunk, Value value);
 
 #endif

c24a285d56e871b5c75faf4c4f2c040ee05db5b7
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-07 20:09:35 +0530

    line information
    storing a separate array of integers that parallels the bytecode. Each
    number in the array is the line number for the corresponding byte in the
    bytecode.

--- chunk.c
+++ chunk.c
@@ -7,26 +7,32 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  chunk->lines = NULL;
   // When we initialize a new chunk, we initialize its constant list too.
   initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  FREE_ARRAY(int, chunk->lines, chunk->capacity); // lines deallocation
   initChunk(chunk);
   // likewise we free the constants when we free the chunk
   freeValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
         GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    // When we allocate or grow the code array, we do the same for the line info
+    // too.
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line; // stored the numebr in array
   chunk->count++;
 }
 

8089acfe2acc402070505d303949c2e586906d7e
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-07 22:05:56 +0530

    instruction execution machine

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
   // we don’t have a compiler yet, we “hand-compile” an instruction in our test
@@ -13,6 +16,9 @@ int main(int argc, const char *argv[]) {
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
+
   freeChunk(&chunk);
   return 0;
 }
---
+++ vm.c
@@ -0,0 +1,42 @@
+#include "vm.h"
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+#include "value.h"
+#include <stdio.h>
+VM vm;
+
+void initVM() {}
+
+void freeVM() {}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.value[READ_BYTE()])
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif /* ifdef DEBUG_TRACE_EXECUTION                                          \
+        */
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      printValue(constant);
+      printf("\n");
+      break;
+    }
+    case OP_RETURN: {
+      return INTERPRET_OK;
+    }
+    }
+  }
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk *chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
